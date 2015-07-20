25b6ab5f04d76d2bce4dd333fd3dec56cfc833a9
Romero Malaquias <romero.malaquias@gmail.com>
2015-07-20 15:59:13 -0300

    add gitignore

--- src/main/groovy/br/ufal/ic/SimilarityTest.groovy
+++ src/main/groovy/br/ufal/ic/SimilarityTest.groovy
@@ -2,16 +2,11 @@ package br.ufal.ic
 
 import br.ufal.ic.commons.ClientRPC
 
-import javax.script.ScriptContext
-import javax.script.ScriptEngine
-import javax.script.ScriptEngineManager
-import javax.script.SimpleScriptContext
-
 /**
  * Created by huxley on 18/07/15.
  */
-class SubmissionTest extends ClientRPC {
-    SubmissionTest(String exchangeName, String key) {
+class SimilarityTest extends ClientRPC {
+    SimilarityTest(String exchangeName, String key) {
         super(exchangeName, key);
     }
 
@@ -20,7 +15,7 @@ class SubmissionTest extends ClientRPC {
     }
 
     public static void main(String[] argv) {
-        SubmissionTest fibonacciRpc = new SubmissionTest("EXCHANGE", "evaluator");
+        SimilarityTest fibonacciRpc = new SimilarityTest("EXCHANGE", "evaluator");
         fibonacciRpc.start();
         fibonacciRpc.call('{"code1":"print \'Ola mundo\';", "code2":"print \'Ola mundo\';", "threshold": 0.9}');
         fibonacciRpc.call('{"code1":"print \'Ola mundo2\';", "code2":"print \'Ola mundo23\';", "threshold": 0.9}');
